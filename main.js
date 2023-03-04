6926087fecb22909f639be303f8ab53d7706adce
silanurs <sarii.silanur@gmail.com>
2023-03-04 22:00:15 +0300

    modified repo

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("function getDataFromForm() {\n    // const cityName = 'pirongia';\n    const input = document.querySelector('.search-box-input');\n    const cityName = input.value;\n  \n    // if not an empty string\n    if (cityName) {\n      // remove whitespace for the api call\n  \n      return cityName\n        .replace(/(\\s+$|^\\s+)/g, '') // remove whitespace from begining and end of string\n        .replace(/(,\\s+)/g, ',') // remove any white space that follows a comma\n        .replace(/(\\s+,)/g, ',') // remove any white space that preceeds a comma\n        .replace(/\\s+/g, '+'); // replace any remaining white space with +, so it works in api call\n    }\n    return '';\n  }\n  \n\n\nasync function getWeatherData(location){\n    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=1f2a375bcd444a9e917183403230403=${location}', {mode:'cors'});\n    const weatherData = await response.json();\n    console.log(weatherData)\n}\nfunction handleInput(){\n    const city = document.querySelector('input');\n    const location = city.value;\n    getWeatherData(location)\n}\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
