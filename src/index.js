a39f22b16d37d83250526fc982bb176753469925
silanurs <sarii.silanur@gmail.com>
2023-03-06 20:47:51 +0300

    fetch API

--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
-function getDataFromForm() {
-    // const cityName = 'pirongia';
-    const input = document.querySelector('.search-box-input');
-    const cityName = input.value;
-  
-    // if not an empty string
-    if (cityName) {
-      // remove whitespace for the api call
-  
-      return cityName
-        .replace(/(\s+$|^\s+)/g, '') // remove whitespace from begining and end of string
-        .replace(/(,\s+)/g, ',') // remove any white space that follows a comma
-        .replace(/(\s+,)/g, ',') // remove any white space that preceeds a comma
-        .replace(/\s+/g, '+'); // replace any remaining white space with +, so it works in api call
-    }
-    return '';
-  }
-  
-
-
+const form = document.querySelector('form');
+const submit = document.querySelector('button[type=submit]');
+form.addEventListener('submit', (e)=> {e.preventDefault()});
+submit.addEventListener('click', handleInput)
 async function getWeatherData(location){
-    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=1f2a375bcd444a9e917183403230403=${location}', {mode:'cors'});
+try{
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=1f2a375bcd444a9e917183403230403&q=${location}`, {mode:'cors'});
     const weatherData = await response.json();
     console.log(weatherData)
 }
+catch(error){
+    console.log(error)
+}
+}
+function displayWeatherData(newData){
+    
+}
 function handleInput(){
-    const city = document.querySelector('input');
-    const location = city.value;
-    getWeatherData(location)
+    const city = document.querySelector('.input');
+    const cityName = city.value;
+    getWeatherData(cityName);
+    form.reset()
 }
