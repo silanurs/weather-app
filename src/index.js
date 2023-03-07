540230878fbba2a9a397ddb79c6ed05d8d7ab230
silanurs <sarii.silanur@gmail.com>
2023-03-07 20:59:41 +0300

    used https instead

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ form.addEventListener('submit', (e)=> {e.preventDefault()});
 submit.addEventListener('click', handleInput)
 async function getWeatherData(location){
 try{
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=1f2a375bcd444a9e917183403230403&q=${location}`, {mode:'cors'});
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1f2a375bcd444a9e917183403230403&q=${location}`, {mode:'cors'});
     const weatherData = await response.json();
     displayWeatherData(weatherData);
 }
